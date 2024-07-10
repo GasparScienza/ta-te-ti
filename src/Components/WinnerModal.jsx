69b69b60155ceddab9888fef84396a425010d16f
Gaspar Scienza <gasparscienza@hotmail.com>
2024-07-10 11:14:06 -0300

    Ta te ti

---
+++ src/Components/WinnerModal.jsx
@@ -0,0 +1,32 @@
+import React from 'react'
+import { Square } from './Square'
+
+function WinnerModal({win, resetGame}) {
+
+    const handleClick = () => {
+        resetGame();
+    }
+
+    if (win === null) return null
+
+    const winnerText = win === false ? 'Empate' : 'Ganó'
+
+  return (
+    <>
+    <section className='winner'>
+        <div className='text'>
+        <h2>{winnerText}</h2>
+        <header className='win'>
+            {win && <Square>{win}</Square>}
+        </header>
+        <footer>
+            <button onClick={handleClick}>Empezar de nuevo</button>
+        </footer>
+        </div>
+    </section>
+    )
+    </>
+  )
+}
+
+export default WinnerModal
